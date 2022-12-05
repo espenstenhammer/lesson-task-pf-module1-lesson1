c922b49f361b8a83aab027415352094586df333f
Espen Stenhammer <espen@lydogfryd.no>
2022-12-05 20:07:43 +0100

    Module Assignment 1 - espen-stenhammer-pf-ma1.js

---
+++ js/espen-stenhammer-pf-ma1.js
@@ -0,0 +1,25 @@
+// question 1Declare and initialise three variables with appropriate names and values. One variable with a number value, one with a boolean value and one with a string value.
+
+var number1 = 5;
+var number2 = "10";
+var result = number1 * number2;
+
+console.log(result);
+
+
+
+
+
+
+
+// question 2 Declare two variables. Assign your first name to one of the variables and your last name to the other. Concatenate (join) the variables, assign them to a third variable and console log the value of the third variable.
+
+var firstName ="Espen";
+var lastName ="Stenhammer";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+
+
+
---
+++ js/script.js
@@ -0,0 +1,39 @@
+// question 1
+
+var number1 = 5;
+var number2 = 10;
+var result = number1 * number2;
+console.log (result);
+
+var pet = "cat" // string
+var petage = 20; // number
+var isFriendly = false; // boolean
+console.log (typeof isFriendly);
+
+var price1 = "10"; //string
+var price2 = 10; // number
+var result = price1 * price2;
+console. log(result);
+
+// question 2
+var firstName ="Espen";
+var lastName ="Stenhammer";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// question 3
+
+var frog = "frog" // string  
+var frog = isFriendly = "yes"; // number
+var frog = noFriendly
+var isFriendly = true; // boolean
+console.log (typeof isFriendly);
+
+// question 4
+
+var orderHasShipped = "true"
+if (orderHasShipped === "true") {
+
+console.log(orderHasShipped);
+} else {console.log(The order did not ship);}
+ 
